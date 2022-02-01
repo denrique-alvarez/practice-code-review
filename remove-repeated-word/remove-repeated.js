68fd979373322bdf0d541d8770678be6cdb63325
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-01 17:48:25 +0100

    Update remove-repeated.js

--- remove-repeated-word/remove-repeated.js
+++ remove-repeated-word/remove-repeated.js
@@ -1,14 +1,15 @@
 /**
- *
+ *@param {string} [sentence] - Original text to be checked for repeated consecutive words.
+ *@returns {string} [result] - The new string without the repeated words.
  */
 
-export const removeConsecutiveDuplicates = (sentence) => {
-  const arr = sentence.split(' ');
-  const result = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== arr[i - 1]) {
-      result.push(arr[i]);
+export const removeConsecutiveDuplicates = (sentence = '') => {
+    const arr = sentence.split(' ');
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i - 1]) {
+            result.push(arr[i]);
+        }
     }
-  }
-  return result.join(' ');
+    return result.join(' ');
 };
