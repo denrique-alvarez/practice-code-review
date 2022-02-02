e16a1dfcdadb5d7767c78a3a5e5aa8a02cf0778f
denrique-alvarez <denrique.alvarez@gmail.com>
2022-02-02 12:23:54 +0100

    Update remove-repeated.spec.js

--- remove-repeated-word/remove-repeated.spec.js
+++ remove-repeated-word/remove-repeated.spec.js
@@ -1,26 +1,26 @@
 import { removeConsecutiveDuplicates } from './remove-repeated.js';
 
 describe('Default values', () => {
-    it('" --> ""', () => {
-        const expected = '';
-        const actual = removeConsecutiveDuplicates('');
-        expect(actual).toEqual(expected);
-    });
-    it('No argument --> ""', () => {
-        const expected = '';
-        const actual = removeConsecutiveDuplicates();
-        expect(actual).toEqual(expected);
-    });
+  it('" --> ""', () => {
+    const expected = '';
+    const actual = removeConsecutiveDuplicates('');
+    expect(actual).toEqual(expected);
+  });
+  it('No argument --> ""', () => {
+    const expected = '';
+    const actual = removeConsecutiveDuplicates();
+    expect(actual).toEqual(expected);
+  });
 });
 describe('Repeated words', () => {
-    it('"My name is is John" --> "My name is John"', () => {
-        const expected = 'My name is John';
-        const actual = removeConsecutiveDuplicates('My name is is John');
-        expect(actual).toEqual(expected);
-    });
-    it('"My name name is is John" --> "My name is John"', () => {
-        const expected = 'My name is John';
-        const actual = removeConsecutiveDuplicates('My name name is is John');
-        expect(actual).toEqual(expected);
-    });
+  it('"My name is is John" --> "My name is John"', () => {
+    const expected = 'My name is John';
+    const actual = removeConsecutiveDuplicates('My name is is John');
+    expect(actual).toEqual(expected);
+  });
+  it('"My name name is is John" --> "My name is John"', () => {
+    const expected = 'My name is John';
+    const actual = removeConsecutiveDuplicates('My name name is is John');
+    expect(actual).toEqual(expected);
+  });
 });
